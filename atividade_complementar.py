a0636e58edac356228e7aec981f73e6b445f7ed0
Celio Rocha <40038619+CelioRochadaSilva@users.noreply.github.com>
2022-08-14 20:46:44 -0300

    finalizando teste
    finalizando atividade com arquivo

--- atividade_complementar.py
+++ atividade_complementar.py
@@ -1,16 +1,16 @@
-def partida(list, inicio, fim):
+ef partida(list, inicio, fim): #0(1)
     i = inicio
     pivot = list[fim]
 
-    for j in range(inicio, fim):
+    for j in range(inicio, fim): # O(1)  
 
-        if list[j] <= pivot:
+        if list[j] <= pivot: #0(1)
 
             list[i], list[j] = list[j], list[i]
-            i = i + 1
+            i = i + 1 # ON  
 
-    list[i], list[fim] = list[fim], list[i]
-    return i
+    list[i], list[fim] = list[fim], list[i] #0(1)
+    return i #0(1) 
 
 
 def quickSort(list, inicio=0, fim=None):
@@ -19,10 +19,10 @@ def quickSort(list, inicio=0, fim=None):
     if inicio < fim:
         pivot = partida(list, inicio, fim)
         quickSort(list, inicio, pivot - 1)
-        quickSort(list, pivot + 1, fim)
+        quickSort(list, pivot + 1, fim) # ON 
 
 
-ar = [3, 0, 1, 8, 7, 2, 5, 4, 9, 6]
+ar = [3, 0, 1, 8, 7, 2, 5, 4, 9, 6]  # LISTA DESORDENADA#
 n = len(ar)
 print(ar, " lista não ordenada")
 quickSort(ar)
